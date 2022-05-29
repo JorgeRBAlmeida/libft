8d9942f5478e95eb8145dfb42106468ddaca5a8e
joalmeid <jorgerodrigo.b.a@gmail.com>
2022-05-29 20:32:43 -0300

    add more tests

--- main.c
+++ main.c
@@ -3,17 +3,50 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: joalmeid <joalmeid@student.42.fr>          +#+  +:+       +#+        */
+/*   By: joalmeid <joalmeid@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/16 11:32:24 by joalmeid          #+#    #+#             */
-/*   Updated: 2022/05/26 16:13:32 by joalmeid         ###   ########.fr       */
+/*   Updated: 2022/05/29 20:16:29 by joalmeid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdio.h>
 #include <strings.h>
+#include <string.h>
 #include <ctype.h>
+
+static void	print_arr(char *arr, int n)
+{
+	int	i;
+
+	i = 0;
+	while (i < n)
+	{
+		printf("%i - %p\tvalue = '%c'\n", i, (void *)&arr[i], arr[i]);
+		i ++;
+	}
+}
+
+static char	ft_strupper_first(unsigned int i, char c)
+{
+	if (i == 0 && ft_isalpha(c))
+		return (ft_toupper(c));
+	else if (i != 0 && ft_isalpha(c))
+		return (ft_tolower(c));
+	return (c);
+}
+
+static void	ft_strupper_first2(unsigned int i, char *c)
+{
+	char	*out = c;
+	if (!ft_isalpha(*out))
+		return ;
+	if ((*out >= 97 && *out <= 122) && i == 0)
+		*out = *out - 32;
+	else if ((*out >= 65 && *out <= 90) && i != 0)
+		*out = *out + 32;
+}
 
 void	ft_atoi_test(void)
 {
@@ -21,8 +54,10 @@ void	ft_atoi_test(void)
 	char	*str2 = "\t\n\v\f\r +123adfsd";
 	char	*str3 = "2147483647";
 	char	*str4 = "-2147483648";
-	char	*str5 = "9223372036854775807";
-	char	*str6 = "-9223372036854775808";
+	char	*str5 = "";
+	char	*str6 = "-92233";
+	char	*str7 = "-1";
+	char	*str8 = "8";
 
 	printf("string1:\t'%s'\natoi result:\t %d\n", str, atoi(str));
 	printf("ft_atoi result:\t %d\n\n", ft_atoi(str));
@@ -41,19 +76,13 @@ void	ft_atoi_test(void)
 
 	printf("string6:\t'%s'\natoi result:\t %d\n", str6, atoi(str6));
 	printf("ft_atoi result:\t %d\n\n", ft_atoi(str6));
+
+	printf("string7:\t'%s'\natoi result:\t %d\n", str7, atoi(str7));
+	printf("ft_atoi result:\t %d\n\n", ft_atoi(str7));
+
+	printf("string8:\t'%s'\natoi result:\t %d\n", str8, atoi(str8));
+	printf("ft_atoi result:\t %d\n\n", ft_atoi(str8));
 } 
-
-static void	print_arr(char *arr, int n)
-{
-	int	i;
-
-	i = 0;
-	while (i < n)
-	{
-		printf("%i - %p\tvalue = '%c'\n", i, (void *)&arr[i], arr[i]);
-		i ++;
-	}
-}
 
 void	ft_bzero_test(void)
 {
@@ -77,16 +106,274 @@ void	ft_bzero_test(void)
 	print_arr(s3, num);
 }
 
+void	ft_calloc_test(void)
+{
+	size_t	num1 = 22234233;
+	size_t	size1 = 8;
+	void	*buf1 = ft_calloc(num1, size1);
+	size_t	num2 = 0;
+	size_t	size2 = 0;
+	void	*buf2 = ft_calloc(num2, size2);
+	size_t	num3 = -1212;
+	size_t	size3 = 3;
+	void	*buf3 = calloc(num3, size3);
+
+	if (buf1 == NULL)
+		printf("Memory allocation failed.\n");
+	else
+		printf("Memory was allocated.\n");
+
+	if (buf2 == NULL)
+		printf("Memory allocation failed.\n");
+	else
+		printf("Memory was allocated.\n");
+
+	if (buf3 == NULL)
+		printf("Memory allocation failed. And it has to\n");
+	else
+		printf("Memory was allocated. And it's not\n");
+}
+
+void	ft_isalnum_test(void)
+{
+	int	c = 0;
+	int	i = 0;
+
+	while (ft_isascii(c))
+	{
+		if (ft_isalnum(c))
+			printf("%d char: %c ft_isalnum? YES\n", i, c);
+		else
+			printf("%d char: %c ft_isalnum? NO\n", i, c);
+		i ++;
+		c ++;
+	}
+}
+
+void	ft_isalpha_test(void)
+{
+	char	c = 0;
+	int		i = 0;
+
+	while (ft_isascii(c))
+	{
+		if (ft_isalpha(c))
+			printf("%d char: %c ft_isalpha? YES\n", i, c);
+		else
+			printf("%d char: %c ft_isalpha? NO\n", i, c);
+		i ++;
+		c ++;
+	}
+}
+
+void	ft_isdigit_test(void)
+{
+	char	c = 0;
+	int		i = 0;
+
+	while (ft_isascii(c))
+	{
+		if (ft_isdigit(c))
+			printf("%d char: %c ft_isdigit? YES\n", i, c);
+		else
+			printf("%d char: %c ft_isdigit? NO\n", i, c);
+		i ++;
+		c ++;
+	}
+}
+
+void	ft_isprint_test(void)
+{
+	char	c = 0;
+	int		i = 0;
+
+	while (ft_isascii(c))
+	{
+		if (ft_isprint(c))
+			printf("%d char: %c ft_isprint? YES\n", i, c);
+		else
+			printf("%d char: %c ft_isprint? NO\n", i, c);
+		i ++;
+		c ++;
+	}
+}
+
+void	ft_itoa_test(void)
+{
+	int		n = -2147483648;
+	int		n2 = 1;
+	int		n3 = 0;
+	int		n4 = 2147483647;
+	char	*src = ft_itoa(n);
+	char	*src2 = ft_itoa(n2);
+	char	*src3 = ft_itoa(n3);
+	char	*src4 = ft_itoa(n4);
+
+	printf("n1 =\t'%i'\n", n);
+	printf("src1 =\t'%s' %zu\n", src, ft_strlen(src));
+
+	printf("\nn2 =\t'%i'\n", n2);
+	printf("src1 =\t'%s' %zu\n", src2, ft_strlen(src2));
+
+	printf("\nn3 =\t'%i'\n", n3);
+	printf("src1 =\t'%s' %zu\n", src3, ft_strlen(src3));
+
+	printf("\nn4 =\t'%i'\n", n4);
+	printf("src1 =\t'%s' %zu\n", src4, ft_strlen(src4));
+
+}
+
+void	ft_memchr_test(void)
+{
+	char	*src1 = "";
+	char	*src2 = "a";
+	char	*src3 = " \t    testando memchr achando z\t!";
+	char	*src4 = "zzzzzzzzzzzzzzzz";
+	char	*src5 = "ztestando memchr achando";
+	char	*src6 = "testando memchr achandoz";
+	char	*src7 = "testando mem z chr achando";
+	char	c = 'z';
+
+	printf("\nstring1:\t'%s'\nlength de busca:\t%zu\n", src1, ft_strlen(src1));
+	printf("result ft_memchr:\t'%s'\n", (unsigned char *)ft_memchr(src1, c, ft_strlen(src1)));
+	printf("expected:\t\t'(null)'\n");
+
+	printf("string2:\t'%s'\nlength de busca:\t%zu\n", src2, ft_strlen(src2));
+	printf("result ft_memchr:\t'%s'\n", (unsigned char *)ft_memchr(src2, c, ft_strlen(src2)));
+	printf("expected:\t\t'(null)'\n");
+
+	printf("string3:\t'%s'\nlength de busca:\t%zu\n", src3, ft_strlen(src3));
+	printf("result ft_memchr:\t'%s'\n", (unsigned char *)ft_memchr(src3, c, ft_strlen(src3)));
+	printf("expected:\t\t'z\t!'\n");
+
+	printf("string4:\t'%s'\nlength de busca:\t%i\n", src4, 0);
+	printf("result ft_memchr:\t'%s'\n", (unsigned char *)ft_memchr(src4, c, 1));
+	printf("expected:\t\t'(null)'\n");
+
+	printf("string5:\t'%s'\nlength de busca:\t%i\n", src5, 1);
+	printf("result ft_memchr:\t'%s'\n", (unsigned char *)ft_memchr(src5, c, 1));
+	printf("expected:\t\t'ztestando memchr achando'\n");
+
+	printf("string6:\t'%s'\nlength de busca:\t%zu\n", src6, ft_strlen(src6));
+	printf("result ft_memchr:\t'%s'\n", (unsigned char *)ft_memchr(src6, c, ft_strlen(src6)));
+	printf("expected:\t\t'z'\n");
+
+	printf("string7:\t'%s'\nlength de busca:\t%i\n", src7, 14);
+	printf("result ft_memchr:\t'%s'\n", (unsigned char *)ft_memchr(src7, c, 14));
+	printf("expected:\t\t'z chr achando'\n");
+
+	printf("string7:\t'%s'\nlength de busca:\t%i\n", src7, 140);
+	printf("result ft_memchr:\t'%s'\n", (unsigned char *)ft_memchr(src7, c, 140));
+	printf("expected:\t\t'z chr achando'\n");
+}
+
+void	ft_memcmp_test(void)
+{
+	char	*src1 = " \t    testando memchr achando z\t!";
+	char	*src2 = " \t    testando memchr achando z\t!";
+	char	*src3 = "testandoz memchr achando";
+	char	*src4 = "testandot memchr achando";
+	int		num1 = 5;
+	int		num2 = 200;
+	void		*p1 = &num1;
+	void		*p2 = &num1;
+
+	printf("\nstring1:\t'%s'\nstring2:\t'%s'\nlength de cmp:\t'%zu\n'", src1, src2, ft_strlen(src1));
+	printf("result ft_memchr:\t'%i'\n", ft_memcmp(src1, src2, ft_strlen(src1)));
+	printf("expected:\t\t'0'\n");
+
+	printf("\nstring3:\t'%s'\nstring4:\t'%s'\nlength de cmp:\t'%zu\n'", src3, src4, ft_strlen(src3));
+	printf("result ft_memchr:\t'%i'\n", ft_memcmp(src3, src4, ft_strlen(src3)));
+	printf("expected:\t\t'6'\n");
+
+	printf("\nstring3:\t'%s'\nstring4:\t'%s'\nlength de cmp:\t'%i\n", src3, src4, 8);
+	printf("result ft_memchr:\t'%i'\n", ft_memcmp(src3, src4, 8));
+	printf("expected:\t\t'0'\n");
+
+	printf("\nstring3:\t'%s'\nstring4:\t'%s'\nlength de cmp:\t'%i\n", src3, src4, 9);
+	printf("result ft_memchr:\t'%i'\n", ft_memcmp(src3, src4, 9));
+	printf("expected:\t\t'6'\n");
+
+	printf("\nnum1:\t'%i'\nnum2:\t'%i'\nlength de cmp:\t'%i\n", num1, num2, 1);
+	printf("result ft_memchr:\t'%i'\n", ft_memcmp(&num1, &num2, 1));
+	printf("expected:\t\t'-195'\n");
+
+	printf("\nnum1:\t'%p'\nnum2:\t'%p'\nlength de cmp:\t'%i\n", p1, p2, 1);
+	printf("result ft_memchr:\t'%i'\n", ft_memcmp(p1, p2, 1));
+	printf("expected:\t\t'0'\n");
+}
+
+void	ft_memcpy_test(void)
+{
+	char csrc[] = "TestandoMemcpy";
+	char cdest[100];
+	ft_memcpy(cdest, csrc, ft_strlen(csrc)+1);
+	printf("result ft_memcpy:\t %s", cdest);
+  
+	int isrc[] = {10, 20, 30, 40, 50};
+	int n = sizeof(isrc)/sizeof(isrc[0]);
+	int idest[n], i;
+	ft_memcpy(idest, isrc, sizeof(isrc));
+	printf("\nCopied array is ");
+	for (i=0; i<n; i++)
+	printf("%d ", idest[i]);
+}
+
+void	ft_memmovecpy_test(void)
+{
+	char str1[50] = "I am going from Delhi to Gorakhpur";
+	char str2[50] = "I am going from Delhi to Gorakhpur";
+
+	printf( "=========ft_memcpy with overlap=========\n" );
+	printf( "string2 :\t%s\n",str2);
+	printf( "source:\t\t%s\n", str2 + 5 );
+	printf( "destination:\t%s\n", str2 + 11 );
+	ft_memcpy( str2 + 11, str2 + 5, 29 );
+	printf( "result:\t\t%s\n", str2 );
+	printf( "length:\t\t%ld characters\n\n", ft_strlen( str2 ) );
+
+	printf( "=========ft_memmove with overlap=========\n" );
+	printf( "string1:\t%s\n",str1);
+	printf( "source:\t\t%s\n", str1 + 5 );
+	printf( "destination:\t%s\n", str1 + 11 );
+	ft_memmove( str1 + 11, str1 + 5, 29 );
+	printf( "result:\t\t%s\n", str1 );
+	printf( "length:\t\t%ld characters\n\n", ft_strlen( str1 ) );
+}
+
+void	ft_memset_test(void)
+{
+	char	s1[16] = "testando memset";
+	char	s2[16] = "testando memset";
+	int num = sizeof(s1);
+	printf("==========string=========\n");
+	print_arr(s1, num);
+	printf("ft_memset n: length=========\n");
+	printf("\texpected: '##############\\0'\n");
+	ft_memset(s1, '#', ft_strlen(s1));
+	print_arr(s1, num);
+	printf("ft_memset n: 3=========\n");
+	printf("\texpected: '###tando memset\\0'\n");
+	ft_memset(s2, '#', 3);
+	print_arr(s2, num);
+}
+
 void	ft_strnstr_test(void)
 {
 	char *s1 = "see FF your FF return FF now FF";
-	char *s2 = "FFF";
-	size_t max = strlen(s1);
-	char *i1 = ft_strnstr("lorem ipsum dolor sit amet", "dolor", 0);
-	char *i2 = ft_strnstr(s1, s2, max);
+	char *s2 = "FF";
+	char *s3 = "lorem ipsum dolor sit amet";
+	char *s4 = "dolor";
 
-	printf("original:\t%s\n", i1);
-	printf("mine:\t\t%s\n", i2);
+	printf("haystack1:\t\t'%s'\n", s1);
+	printf("needle1:\t\t'%s'\n", s2);
+	printf("length:\t\t\t'%zu'\n", ft_strlen(s1));
+	printf("result ft_strnstr:\t'%s'\n\n", ft_strnstr(s1, s2, ft_strlen(s1)));
+
+	printf("haystack2:\t\t'%s'\n", s3);
+	printf("needle2:\t\t'%s'\n", s4);
+	printf("length:\t\t\t'0'\n");
+	printf("result ft_strnstr:\t'%s'\n", ft_strnstr(s3, s4, 0));
 }
 
 void	ft_substr_test(void)
@@ -129,7 +416,7 @@ void	ft_strlcat_test(void)
 	/* char *src = "the cake is a lie !\0I'm hidden lol\r\n"; //19
 	char dst1[0xF00] = "there is no stars in the sky"; //28
 	char dst2[0xF00] = "there is no stars in the sky"; //28
-	size_t dstsize = ft_strlen("the cake is a lie !\0I'm hidden lol\r\n") + 8; //23 */
+	size_t dstsize = ft_strlen("the cake is a lie !\0I'm hidden lol\r\n") + 8; //23
 
 	char *expec = "rrrrrrlorema\0\0\0";
 	char *dest1 = (char *)malloc(sizeof(*dest1) * 15);
@@ -152,7 +439,7 @@ void	ft_strlcat_test(void)
 	printf("return strlcat:\t\t%zu\n", strlcat(dest2, "lorem", 15));
 	printf("dst string result:\t%s\n\n", dest2);
 	
-	/* printf("\ndstsize value:\t\t%zu\n", dstsize);
+	printf("\ndstsize value:\t\t%zu\n", dstsize);
 	printf("\nreturn ft_strlcat:\t%zu\n", ft_strlcat(dst1, src, dstsize));
 	printf("dst string result:\t%s\n\n", dst1);
 	
@@ -197,12 +484,9 @@ void	ft_strjoin_test(void)
 void	ft_strtrim_test(void)
 {
 	char	*s1 = "   \t  \n\n \t\t  \n\n\nHello \t  Please\n Trim me !\n   \n \n \t\t\n";
-	/* char	*s2 = "tataaatatttataaatata";
+	char	*s2 = "tataaatatttataaatata";
 	char	*s3 = "";
 	char	*s4 = "vern";
-	len s1 = 53
-	
-	 */
 	char	*set = " \t\n";
 
 	printf("string:\t\t'%s'\n", s1);
@@ -210,7 +494,7 @@ void	ft_strtrim_test(void)
 	printf("expected:\t''\n");
 	printf("result:\t\t'%s'\n", ft_strtrim(s1, set));
 
-	/* printf("\nstring:\t\t'%s'\n", s2);
+	printf("\nstring:\t\t'%s'\n", s2);
 	printf("set:\t\t'%s'\n", set);
 	printf("expected:\t''\n");
 	printf("result:\t\t'%s'\n", ft_strtrim(s2, set));
@@ -223,7 +507,7 @@ void	ft_strtrim_test(void)
 	printf("\nstring:\t\t'%s'\n", s4);
 	printf("set:\t\t'%s'\n", set);
 	printf("expected:\t'vern'\n");
-	printf("result:\t\t'%s'\n", ft_strtrim(s4, set)); */
+	printf("result:\t\t'%s'\n", ft_strtrim(s4, set));
 }
 
 void	ft_split_test(void)
@@ -311,40 +595,6 @@ void	ft_split_test(void)
 	i = 0;
 }
 
-void	ft_itoa_test(void)
-{
-	int		n = -2147483648;
-	int		n2 = 1;
-	int		n3 = 0;
-	int		n4 = 2147483647;
-	char	*src = ft_itoa(n);
-	char	*src2 = ft_itoa(n2);
-	char	*src3 = ft_itoa(n3);
-	char	*src4 = ft_itoa(n4);
-
-	printf("n1 =\t'%i'\n", n);
-	printf("src1 =\t'%s' %zu\n", src, ft_strlen(src));
-
-	printf("\nn2 =\t'%i'\n", n2);
-	printf("src1 =\t'%s' %zu\n", src2, ft_strlen(src2));
-
-	printf("\nn3 =\t'%i'\n", n3);
-	printf("src1 =\t'%s' %zu\n", src3, ft_strlen(src3));
-
-	printf("\nn4 =\t'%i'\n", n4);
-	printf("src1 =\t'%s' %zu\n", src4, ft_strlen(src4));
-
-}
-
-static char	ft_strupper_first(unsigned int i, char c)
-{
-	if (i == 0 && ft_isalpha(c))
-		return (ft_toupper(c));
-	else if (i != 0 && ft_isalpha(c))
-		return (ft_tolower(c));
-	return (c);
-}
-
 void	ft_strmapi_test(void)
 {
 	char	*str1 = "nEW TeSt for mAPI.";
@@ -362,17 +612,6 @@ void	ft_strmapi_test(void)
 	printf("\nstr3:\t\t'%s'\n", str3);
 	printf("expected:\t''\n");
 	printf("result:\t\t'%s'\n", ft_strmapi(str3, ft_strupper_first));
-}
-
-static void	ft_strupper_first2(unsigned int i, char *c)
-{
-	char	*out = c;
-	if (!ft_isalpha(*out))
-		return ;
-	if ((*out >= 97 && *out <= 122) && i == 0)
-		*out = *out - 32;
-	else if ((*out >= 65 && *out <= 90) && i != 0)
-		*out = *out + 32;
 }
 
 void	ft_striteri_test(void)
@@ -455,6 +694,6 @@ void	ft_putnbr_fd_test(void)
 
 int	main(void)
 {
-	ft_split_test();
+	ft_strnstr_test();
 	return (0);
 }
