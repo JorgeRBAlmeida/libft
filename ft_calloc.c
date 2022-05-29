b5183330ba24fa9518473fc3bb47601ea42af31d
joalmeid <jorgerodrigo.b.a@gmail.com>
2022-05-29 20:27:56 -0300

    rename variable out to buffer

--- ft_calloc.c
+++ ft_calloc.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_calloc.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: joalmeid <joalmeid@student.42.fr>          +#+  +:+       +#+        */
+/*   By: joalmeid <joalmeid@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/15 16:44:53 by joalmeid          #+#    #+#             */
-/*   Updated: 2022/05/26 10:44:01 by joalmeid         ###   ########.fr       */
+/*   Updated: 2022/05/29 10:36:49 by joalmeid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,18 @@
 
 void	*ft_calloc(size_t num, size_t size)
 {
-	void	*out;
+	void	*buffer;
 	long	total;
 
-	total = num * size;
 	if (num == 0 || size == 0)
 	{
 		num = 1;
 		size = 1;
 	}
-	out = malloc(total);
-	if (out == NULL)
+	total = num * size;
+	buffer = malloc(total);
+	if (buffer == NULL)
 		return (NULL);
-	ft_bzero(out, total);
-	return (out);
+	ft_bzero(buffer, total);
+	return (buffer);
 }
